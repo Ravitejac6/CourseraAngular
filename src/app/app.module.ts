3b21d0dc8eb48ab50c14c722c5a8ff85deae359f
Ravitejac6 <ravitejapenugonda.3@gmail.com>
2020-05-02 16:23:42 +0530

    SinglePage app part-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppComponent } from "./app.component";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
-import { DishService } from "./services/dish.service";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
+
+import { DishService } from "./services/dish.service";
+import { PromotionService } from "./services/promotion.service";
 
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 @NgModule({
@@ -42,7 +44,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatCardModule,
     FlexLayoutModule,
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
