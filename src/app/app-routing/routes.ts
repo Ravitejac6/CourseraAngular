dcd947b9f775250e3daa3bbf44eb9619a7d1dfce
Ravitejac6 <ravitejapenugonda.3@gmail.com>
2020-05-03 13:04:28 +0530

    Assignment-2 done

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,5 +11,6 @@ export const routes: Routes = [
   { path: "menu", component: MenuComponent },
   { path: "dishdetail/:id", component: DishdetailComponent }, // Route parameters
   { path: "contactus", component: ContactComponent },
+  { path: "aboutus", component: AboutComponent },
   { path: "", redirectTo: "/home", pathMatch: "full" },
 ];
